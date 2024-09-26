0a0f9f7bc84014bf9a03adcbb7189b7306eb8639
Sandeep Kashyap 😻 <155357259+Sandeepkr07@users.noreply.github.com>
2024-09-26 10:38:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "24558120"))
+API_HASH = getenv("API_HASH", "85bd1a317c44733e6620ec82a7053fe9")
+BOT_TOKEN = getenv("BOT_TOKEN", "7264260495:AAG_nyJj_vAkQ_bS_8ogVxib8py9iz7gLuY")
+OWNER_ID = int(getenv("OWNER_ID", "6005294654"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://sandeepkrbth64:NbPbc81D9Z7CXhLj@sandeepkr.5zk8q.mongodb.net/?retryWrites=true&w=majority&appName=Sandeepkr")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002381172477"))
+FORCESUB = getenv("FORCESUB", "-1002325159591")
